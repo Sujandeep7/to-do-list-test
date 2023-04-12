d74a3719d7da00377b8f26b5bd0b43ea15e73ca5
Sujandeep7 <skshrestha6@myseneca.ca>
2023-04-12 13:09:00 -0400

    initial

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  database: {
+    user: 'fdfeigut',
+    host: 'isilo.db.elephantsql.com',
+    database: 'fdfeigut',
+    password: 'SiKSQ_y4GHa1Wqng39CYsxjiJ0a85BhC',
+    port: 5432,
+  },
+};
